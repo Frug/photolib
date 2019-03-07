7b52d0254c9f2799a51e38795eac9705149001d5
Philip Nicolcev <frugen@gmail.com>
2019-03-07 00:10:37 -0500

    Fix port

--- dump-photos.sh
+++ dump-photos.sh
@@ -4,8 +4,8 @@ set -e
 
 for i in {1..10}
 do
-	curl -X POST http://localhost:8080/photos -H 'Content-Type: application/json' -d '{"url":"https://placekitten.com/200/287", "title": "Test '$i'"}'
-	curl -X POST http://localhost:8080/photos -H 'Content-Type: application/json' -d '{"url":"https://placekitten.com/220/285", "title": "B Test '$i'"}'
-	curl -X POST http://localhost:8080/photos -H 'Content-Type: application/json' -d '{"url":"https://placekitten.com/210/200", "title": "C Test '$i'"}'
-	curl -X POST http://localhost:8080/photos -H 'Content-Type: application/json' -d '{"url":"https://placekitten.com/300/210", "title": "Wide Test '$i'"}'
+	curl -X POST http://localhost:8081/photos -H 'Content-Type: application/json' -d '{"url":"https://placekitten.com/200/287", "title": "Test '$i'"}'
+	curl -X POST http://localhost:8081/photos -H 'Content-Type: application/json' -d '{"url":"https://placekitten.com/220/285", "title": "B Test '$i'"}'
+	curl -X POST http://localhost:8081/photos -H 'Content-Type: application/json' -d '{"url":"https://placekitten.com/210/200", "title": "C Test '$i'"}'
+	curl -X POST http://localhost:8081/photos -H 'Content-Type: application/json' -d '{"url":"https://placekitten.com/300/210", "title": "Wide Test '$i'"}'
 done
